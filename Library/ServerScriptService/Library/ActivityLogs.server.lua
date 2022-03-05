5ab89a3fa3474e17132f93f2fd60ae433fd4191a
tycoonlover1359 <6766257+tycoonlover1359@users.noreply.github.com>
2022-03-05 10:53:42 -0800

    Update ActivityLogs.server.lua

--- Library/ServerScriptService/Library/ActivityLogs.server.lua
+++ Library/ServerScriptService/Library/ActivityLogs.server.lua
@@ -5,65 +5,51 @@ if RunService:IsStudio() or game.PrivateServerOwnerId ~= 0 then
 end
 
 local GROUP_ID = 2735192
-local MIN_ROLE_TO_TRACK = 13
-local WebhookURL = ""
+local RANKS_TO_TRACK = {
+	100,
+	101,
+	102,
+	103,
+	200,
+	250,
+	252,
+	253,
+	254,
+	255
+}
 
 local HttpService = game:GetService("HttpService")
 local Players = game:GetService("Players")
 local playerInfo = {}
 
-local function getTimestamp(epochTime)
-    local currentTime = os.date("!*t", epochTime)
-    return ("%.2d-%.2d-%.2dT%.2d:%.2d:%.2dZ"):format(currentTime.year, currentTime.month, currentTime.day, currentTime.hour, currentTime.min, currentTime.sec)
-end
-
-local function secondsToClock(seconds)
-    if seconds <= 0 then
-        return "00h 00m 00s"
-    else
-        local hours = string.format("%.2d", math.floor(seconds / 3600))
-        local mins = string.format("%.2d", math.floor(seconds / 60 - (hours * 60)))
-        local secs = string.format("%.2d", math.floor(seconds - hours * 3600 - mins * 60))
-        return ("%sh %sm %ss"):format(hours, mins, secs)
-    end
-end
-
 Players.PlayerAdded:Connect(function(player)
-    if player:GetRankInGroup(GROUP_ID) < MIN_ROLE_TO_TRACK then return end
-    playerInfo[player] = {
-        joined = os.time(),
-        role = player:GetRoleInGroup(GROUP_ID),
-    }
+	if not table.find(RANKS_TO_TRACK, player:GetRankInGroup(GROUP_ID)) then return end
+	playerInfo[player.UserId] = os.time()
 end)
 
 Players.PlayerRemoving:Connect(function(player)
-    local info = playerInfo[player]
-    if not info then return end
-    playerInfo[player] = nil
-    local data = {
-        ["embeds"] = {
-            {
-                ["fields"] = {
-                    {
-                        ["name"] = "Username",
-                        ["value"] = player.Name
-                    },
-                    {
-                        ["name"] = "Rank",
-                        ["value"] = info.role
-                    },
-                    {
-                        ["name"] = "Time Stayed",
-                        ["value"] = secondsToClock(os.time() - info.joined)
-                    }
-                },
-                ["footer"] = {
-                    ["text"] = "Roblox Library 2021"
-                },
-                ["timestamp"] = getTimestamp()
-            }
-        }
-    }
-    local finalData = HttpService:JSONEncode(data)
-    HttpService:PostAsync(WebhookURL, finalData)
+	local info = playerInfo[player.UserId]
+	if not info then return end
+	playerInfo[player.UserId] = nil
+	local payload = {
+		["PlayerId"] = tostring(player.UserId),
+		["JoinTime"] = tostring(info),
+		["LeaveTime"] = tostring(os.time()),
+		["Username"] = tostring(player.Name),
+		["DisplayName"] = tostring(player.DisplayName),
+		["Rank"] = tostring(player:GetRoleInGroup(GROUP_ID))
+	}
+	local payloadJson = HttpService:JSONEncode(payload)
+	local success, err = pcall(function()
+		return HttpService:RequestAsync(
+			{
+				["Url"] = "https://dyga35t7cc.execute-api.us-west-2.amazonaws.com/dev/activity-logs",
+				["Method"] = "POST",
+				["Headers"] = {
+					["Content-Type"] = "application/json"
+				},
+				["Body"] = payloadJson
+			}
+		)
+	end)
 end)
